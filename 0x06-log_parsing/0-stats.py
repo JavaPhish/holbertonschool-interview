b1e4b8a83e4568256cc8cefb1ae34eb006f95804
JavaPhish <cclements3150@gmail.com>
2021-01-28 17:24:47 -0500

    Created print data function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,6 +2,8 @@
 """ Reads stdin """
 
 import sys
+import traceback
+
 
 r_lines = 0
 t_file_size = 0
@@ -16,6 +18,13 @@ s_codes = {'200': 0,
            '500': 0}
 
 
+def print_data():
+    """ Prints one iteration to stdout """
+    print("File size: {}".format(t_file_size))
+    for key in sorted(s_codes.keys()):
+        if (s_codes[key] != 0):
+            print("{}: {}".format(key, s_codes[key]))
+
 try:
     for line in sys.stdin:
 
@@ -26,27 +35,26 @@ try:
             continue
 
         """ Add file size """
-        t_file_size += int(line[-1])
+        """ If it fails we got a bad code, ignore it """
+        try:
+            t_file_size += int(line[-1])
 
-        """ Check status code """
-        if (line[-2] in (s_codes.keys())):
-            s_codes[line[-2]] += 1
-        else:
+            """ Check status code """
+            if (line[-2] in (s_codes.keys())):
+                s_codes[line[-2]] += 1
+            else:
+                continue
+        except:
             continue
 
         """ increase line count """
         r_lines += 1
-        if (r_lines == 10):
-            print("File size: {}".format(t_file_size))
-            for key in sorted(s_codes.keys()):
-                if (s_codes[key] != 0):
-                    print("{}: {}".format(key, s_codes[key]))
+        if (r_lines == 9):
+            print_data()
             r_lines = 0
 
+    print_data()
+
 except KeyboardInterrupt:
-    print("File size: {}".format(t_file_size))
-    for key in sorted(s_codes.keys()):
-        if (s_codes[key] != 0):
-            print("{}: {}".format(key, s_codes[key]))
-        r_lines = 0
+    print_data()
     raise
