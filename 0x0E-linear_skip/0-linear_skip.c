2ac338b14b2fa76e711c2747f8e70fec99b54e68
JavaPhish <cclements3150@gmail.com>
2021-04-01 21:45:25 -0400

    Merged wrong thing

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "search.h"
 
-
 /**
  * linear_skip - Linear searches a skip list
  * @list: The list
@@ -12,43 +11,44 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *node;
-	skiplist_t *tail;
-
-	node = list;
+	skiplist_t *node = list->express, *tail = list;
 
 	if (!list)
 		return (NULL);
-
-
 	/* Find the correct express node */
 	while (node)
 	{
 		printf("Value checked at index [%d] = [%d]\n", (int)node->index, node->n);
-		tail = node;
-		node = node->express;
-
-		if (!node)
-			return (NULL);
-
-		if (value < node->n)
+		if (node->n >= value)
 		{
 			printf("Value found between indexes [%d] and [%d]\n",
-				 (int)tail->index, (int)node->index);
+			       (int)tail->index, (int)node->index);
 			break;
 		}
+		if (node->express == NULL)
+		{
+			tail = node;
+			while (node->next)
+				node = node->next;
+			printf("Value found between indexes [%d] and [%d]\n",
+			       (int)tail->index, (int)node->index);
+			break;
+		}
+		tail = node;
+		node = node->express;
 	}
-
-	/* Search the section indicated by the express lane */
+	/* Search the section indicated by the express lanes */
 	while (tail)
 	{
 		if (tail->n == value)
+		{
+			printf("Value checked at index [%d] = [%d]\n",
+				 (int)tail->index, tail->n);
 			return (tail);
-
-		printf("Value checked at index [%d] = [%d]\n", (int)tail->index, tail->n);
-
+		}
+		printf("Value checked at index [%d] = [%d]\n",
+			 (int)tail->index, tail->n);
 		tail = tail->next;
 	}
-
 	return (NULL);
 }
