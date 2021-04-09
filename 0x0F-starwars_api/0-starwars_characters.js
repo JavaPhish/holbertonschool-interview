76fa693f08ed2a572afdb36abfc71e660d7d7d27
JavaPhish <cclements3150@gmail.com>
2021-04-08 22:42:41 -0400

    Added async stuff

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,14 +3,19 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (err, res, body) {
+request(url, async function (err, res, body) {
   if (err) throw err;
   const dataIds = JSON.parse(body).characters;
-  
-  dataIds.forEach(function (item, index) {
-    request(item, function (err, res, body) {
-      if (err) throw err;
-      console.log(JSON.parse(body).name);
+
+  for (const c of dataIds) {
+    await new Promise((resolve) => {
+      request(c, function (err, res, body) {
+        if (err) {
+          throw err;
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
