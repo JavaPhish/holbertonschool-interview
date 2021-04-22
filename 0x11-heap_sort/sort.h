831a19d05ab61e26c3fc2c590b4cd94e94b4d292
JavaPhish <cclements3150@gmail.com>
2021-04-22 19:03:47 -0400

    Heap sort

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -3,6 +3,6 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void sift_down(int *array, int heap_len, int start, int len);
+void sift_down(int *array, int heap_len, unsigned int start, int len);
 
 #endif
