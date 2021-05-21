f03d7e829f34ac625bd986ab9d369b01ed41c544
JavaPhish <cclements3150@gmail.com>
2021-05-20 23:25:03 -0400

    Header

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void error(void);
+int length(char *arr);
+void validate(char *str);
+void multiply(char *s1, char *s2);
+
+#endif
+
