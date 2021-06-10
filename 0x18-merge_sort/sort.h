46104b35872195b0c0c9ed2be7f0311d24bd1d25
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2021-06-10 05:19:35 -0400

    Update sort.h

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -2,6 +2,8 @@
 #define _SORT_H_
 
 void print_array(const int *array, size_t size);
+void merge_driver(int *array, int *temp, int start, int end);
+void merge(int *array, int *temp, int start, int mid, int end);
 void merge_sort(int *array, size_t size);
 
 #endif
