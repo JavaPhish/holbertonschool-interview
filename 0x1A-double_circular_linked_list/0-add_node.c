e7b1b1f464051c0538641433c76a1e98c3bc5052
JavaPhish <cclements3150@gmail.com>
2021-07-01 08:52:10 -0400

    Fix

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
 
@@ -14,40 +15,41 @@ List *add_node_end(List **list, char *str)
 {
 	List *n_start, *node, *new_node;
 
-	/* No nodes present in the list */
+	if (!list || !str)
+		return (NULL);
 	if ((*list) == NULL)
 	{
 		(*list) = malloc(sizeof(List));
-		(*list)->str = str;
-		printf("1\n");
+		if (!(*list))
+			return (NULL);
+		(*list)->str = strdup(str);
 		return (*list);
 	}
 
-	/* Only one node exists (Edge case) */
 	if ((*list)->next == NULL)
 	{
 		node = malloc(sizeof(List));
-		node->str = str;
+		if (!node)
+			return (NULL);
+		node->str = strdup(str);
 		node->prev = (*list);
 		node->next = (*list);
 		(*list)->next = node;
 		return (node);
 	}
-
-	/* Reference to the start node and create a travel node */
 	n_start = (*list);
 	node = (*list);
-
 	while (node->next != n_start)
 		node = node->next;
 
-	/* Create and link the node in between the end and start nodes */
 	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 	node->next = new_node;
 	new_node->prev = node;
 	new_node->next = n_start;
 	n_start->prev = new_node;
-	new_node->str = str;
+	new_node->str = strdup(str);
 
 	return (new_node);
 }
@@ -63,19 +65,21 @@ List *add_node_begin(List **list, char *str)
 {
 	List *head, *tail, *new_node;
 
-	/* No nodes present in the list */
+	if (!list || !str)
+		return (NULL);
 	if ((*list) == NULL)
 	{
-		/* Empty list. Create a new node and set it as the list */
 		(*list) = malloc(sizeof(List));
+		if (!list)
+			return (NULL);
 		(*list)->str = str;
 		return (*list);
 	}
-
-	/* Only one node exists (Edge case) */
 	if ((*list)->next == NULL)
 	{
 		new_node = malloc(sizeof(List));
+		if (!new_node)
+			return (NULL);
 		new_node->str = str;
 		(*list)->next = new_node;
 		(*list)->prev = new_node;
@@ -83,20 +87,18 @@ List *add_node_begin(List **list, char *str)
 		new_node->prev = (*list);
 		(*list) = new_node;
 		list = &new_node;
-
 		return (new_node);
 	}
-
 	head = (*list);
 	tail = (*list)->prev;
-
 	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 	new_node->str = str;
 	new_node->next = head;
 	new_node->prev = tail;
 	tail->next = new_node;
 	head->prev = new_node;
-
 	(*list) = new_node;
 	list = &new_node;
 
